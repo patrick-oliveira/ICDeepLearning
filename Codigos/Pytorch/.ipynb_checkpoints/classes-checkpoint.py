ea0fe033081407283ccf57eee744746c4a100e16
Patrick Oliveira <patrick.oliveira@aluno.ufabc.edu.br>
2020-07-10 16:15:51 -0300

    .

--- Codigos/Pytorch/.ipynb_checkpoints/classes-checkpoint.py
+++ Codigos/Pytorch/.ipynb_checkpoints/classes-checkpoint.py
@@ -92,12 +92,20 @@ import torch.nn as nn
 
 def activation(activationFunction):
     activations = nn.ModuleDict([
-        ['lrelu', nn.LeakyReLU()],
+        ['lrelu', nn.LeakyReLU(negative_slope = 0.01, inplace = True)],
         ['relu', nn.ReLU(inplace = True)],
-        ['identity', nn.Identity()],
-        ['sigmoid', nn.Sigmoid()]
+        ['none', nn.Identity()],
+        ['selu', nn.SELU(inplace = True)],
+        ['sigmoid', nn.Sigmoid()],
     ])
     return activations[activationFunction]
+
+
+class Conv2dAuto(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.padding = (self.kernel_size[0] // 2,
+                        self.kernel_size[1] // 2) # dynamic add padding based on the kernel_size
 
 def ConvBlock(in_size, out_size, activFunc = 'relu', *args, **kwargs):
 #     if padding == None: padding = kernel // 2
@@ -106,24 +114,113 @@ def ConvBlock(in_size, out_size, activFunc = 'relu', *args, **kwargs):
                          activation(activFunc))
     
 
-    
 def DeconvBlock(in_size, out_size, activFunc = 'relu', *args, **kwargs):
 #     if padding == None: padding = kernel // 2
     return nn.Sequential(nn.ConvTranspose2d(in_size, out_size, *args, **kwargs),
                          nn.BatchNorm2d(out_size),
                          activation(activFunc))
 
-class ConvBlocks(nn.Module):
-    def __init__(self, encSizes, *args, **kwargs):
+# Faça funções para blocos de convolução especificando funções intermediárias
+
+# def ConvBlocks(encSizes, middleActivationFunc = 'relu', *args, **kwargs):
+#     return [ConvBlock(inputSize, outputSize, middleActivationFunc, *args, **kwargs) for
+#                                      inputSize, outputSize in zip(encSizes, encSizes[1:])]
+
+def DeconvBlocks(decSizes, middleActivationFunc = 'relu', *args, **kwargs):
+    return [ConvBlock(inputSize, outputSize, middleActivationFunc, *args, **kwargs) for
+                                     inputSize, outputSize in zip(encSizes, encSizes[1:])]
+
+
+# class ConvBlocks(encSizes, middleActivationFunc = 'relu', endActivationFunc = 'identity' *args, **kwargs):
+#     return nn.Sequential(*[ConvBlock(inputSize, outputSize, middleActivationFunc, *args, **kwargs) for
+#                                      inputSize, outputSize in zip(encSizes, encSizes[1:])],
+#                          activation(endActivationFunc))
+
+        
+# class DeconvBlocks(decSizes, middleActivationFunc = 'relu', endActivationFunc = 'identity', *args, **kwargs):
+#     return nn.Sequential(*[ConvBlock(inputSize, outputSize, middleActivationFunc, *args, **kwargs) for
+#                                      inputSize, outputSize in zip(encSizes, encSizes[1:])],
+#                          activation(endActivationFunc))
+
+def ConvBlocks(inSizes, outSizes, middleActivationFunc = 'relu', *args, **kwargs):
+    return nn.Sequential(*[ConvBlock(inputSize, outputSize, middleActivationFunc, *args, **kwargs) for
+                                     inputSize, outputSize in zip(inSizes, outSizes)])
+
+class DenseBlock(nn.Module):
+    def __init__(self, firstChannels, fixedSizeOutChannels, layerSize, kernel_size):
         super().__init__()
-        self.convblocks = nn.Sequential(*[ConvBlock(inputSize, outputSize, *args, **kwargs) for
-                                          inputSize, outputSize in zip(encSizes, encSizes[1:])])
+        
+        inSizes = [firstChannels, fixedSizeOutChannels]
+        outSizes = [fixedSizeOutChannels]*layerSize
+        self.relu = nn.ReLU()
+        for i in range(layerSize - 2):
+            inSizes.append(inSizes[-1] + fixedSizeOutChannels)
+            
+        self.BN = nn.BatchNorm2d(num_features = firstChannels)
+        self.ConvLayers = ConvBlocks(inSizes, outSizes, 
+                                     kernel_size = kernel_size, stride = 1, padding = kernel_size//2)
     
     def forward(self, X):
-        return self.convblocks(X)
-        
-class DeconvBlocks(nn.Module):
-    def __init__(self, decSizes, *args, **kwargs):
+        X = self.BN(X)
+        output = [self.ConvLayers[0](X)]
+        dense  = self.relu(torch.cat(output, 1))
+        for conv in self.ConvLayers[1:]:
+            Y = conv(dense)
+            output.append(Y)
+            dense = nn.ReLU(torch.cat(output, 1))
+            
+        return dense    
+    
+class TransitionLayer(nn.Module):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.decovblocks = nn.Sequential(*[DeconvBlock(inputSize, outputSize, *args, **kwargs) for
-                                          inputSize, outputSize in zip(decSizes, decSizes[1:])])
+        
+        self.relu = nn.ReLU(inplace = True)
+        self.bn   = nn.BatchNorm2d(num_features = out_channels)
+        self.conv = nn.Conv2d(in_channels = in_channels, out_channels = out_channels,
+                              kernel_size = 1, bias = False)
+        self.avg_pool = nn.AvgPool2d(kernel_size = 2, stride = 2, padding = 0)
+        
+    def forward(self, X):
+        bn = self.bn(self.relu(self.conv(X)))
+        out = self.avg_pool(bn)
+        
+        return out
+    
+    
+class DenseNet(nn.Module):
+    def __init__(self, nr_classes):
+        super(DenseNet, self).__init__()
+        
+        self.FirstConv = nn.Sequential(
+                nn.ReLU(),
+                nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 3, bias = False)
+        )
+        
+        self.DenseLayer1 = nn.Sequential( # 160 = 32 * 5
+                DenseBlock(64, 32, 5, 3),
+                TransitionLayer(160, 128) 
+        )
+        self.DenseLayer2 = nn.Sequential(
+                DenseBlock(128, 32, 5, 3),
+                TransitionLayer(160, 128)
+        )
+        self.DenseLayer3 = nn.Sequential(
+                DenseBlock(128, 32, 5, 3),
+                TransitionLayer(160, 64)
+        )
+        self.BN = nn.BatchNorm2d(num_features = 64)
+        self.Classifier = nn.Sequential(
+                nn.Linear(64*4*4, 512),
+                nn.Linear(512, nr_classes)
+        )
+        
+    def forward(self, X):
+        X = self.FirstConv(X)
+        X = self.DenseLayer1(X)
+        X = self.DenseLayer2(X)
+        X = self.DenseLayer3(X)
+        X = self.BN(X)
+        x.view(-1, 64*4*4)
+        X = self.Classifier(X)
+        return X
