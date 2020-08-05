567bc97a1f3c7009f788c19dbf7641d250b0f6c4
Patrick Oliveira <patrick.oliveira@aluno.ufabc.edu.br>
2020-08-05 07:15:57 -0300

    .

---
+++ Codigos/Análise de BCI/Scripts/.ipynb_checkpoints/NNCreationAux-checkpoint.py
@@ -0,0 +1,72 @@
+from torchvision import models
+import torch.nn as nn
+
+def resnet18(numClasses, pretrained = True):
+    model = models.resnet18(pretrained = pretrained)
+    model.fc = nn.Linear(512, numClasses)
+    return model
+
+def resnet34(numClasses, pretrained = True):
+    model = models.resnet34(pretrained = pretrained)
+    model.fc = nn.Linear(512, numClasses)
+    return model
+
+def resnet50(numClasses, pretrained = True):
+    model = models.resnet50(pretrained = pretrained)
+    model.fc = nn.Linear(2048, numClasses)
+    return model
+
+def resnet101(numClasses, pretrained = True):
+    model = models.resnet101(pretrained = pretrained)
+    model.fc = nn.Linear(2048, numClasses)
+    return model
+
+def resnet152(numClasses, pretrained = True):
+    model = models.resnet152(pretrained = pretrained)
+    model.fc = nn.Linear(2048, numClasses)
+    return model
+
+def densenet121(numClasses, pretrained = True):
+    model = models.densenet121(pretrained = pretrained)
+    model.classifier = nn.Linear(1024, numClasses)
+    return model
+
+def densenet161(numClasses, pretrained = True):
+    model = models.densenet161(pretrained = pretrained)
+    model.classifier = nn.Linear(2208, numClasses)
+    return model
+
+def densenet169(numClasses, pretrained = True):
+    model = models.densenet169(pretrained = pretrained)
+    model.classifier = nn.Linear(1664, numClasses)
+    return model
+
+def densenet201(numClasses, pretrained = True):
+    model = models.densenet201(pretrained = pretrained)
+    model.classifier = nn.Linear(1920, numClasses)
+    return model
+
+def alexnet(numClasses, pretrained = True):
+    model = models.alexnet(pretrained = pretrained)
+    model.classifier[6] = nn.Linear(4096, numClasses)
+    return model
+
+def googlenet(numClasses, pretrained = True):
+    model = models.googlenet(pretrained = pretrained)
+    model.fc = nn.Linear(1024, numClasses)
+
+def model_selection(modelName, numClasses, pretrained = True):
+    model_dict = {
+        'resnet18': resnet18,
+        'resnet34': resnet34,
+        'resnet50': resnet50,
+        'resnet101': resnet101,
+        'resnet152': resnet152,
+        'densenet121': densenet121,
+        'densenet161': densenet161,
+        'densenet169': densenet169,
+        'densenet201': densenet201,
+        'alexnet': alexnet,
+        'googlenet': googlenet
+    }
+    return model_dict[modelName](numClasses, pretrained)
--- Codigos/Análise de BCI/Scripts/NNUtils.py
+++ Codigos/Análise de BCI/Scripts/NNUtils.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('Scripts')
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,157 +7,19 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 
 import pandas as pd
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
 import numpy as np
+import matplotlib.pyplot as plt; plt.style.use('ggplot')
 
-from utils import path
-from utils import createInputFigure
+from sklearn.model_selection import KFold
 
-from sklearn import preprocessing
-import os
-import time
 import copy
+import time
 
-image_size = 224
-batch_size = 64
-workers = 1
 ngpu = 1
-validation_split = .2
-train_split = 0
-shuffle_dataset = True
-random_seed = 42
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
-def getData(dataType, batchSize):
-    folder = 'GAF_Combinados'
-    figPath = path('C:\\Users\\Patrick\\Documents\\GitHub\\ICDeepLearning\\Data\\'+folder+'\\'+dataType)
 
-    dataset = datasets.ImageFolder(root = figPath(),
-                                  transform = transforms.Compose([
-                                      transforms.Resize(image_size),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize((0.5, 0.5, 0.5),
-                                                           (0.5, 0.5, 0.5))
-                                  ]))
-
-    class_names = dataset.classes
-
-    dataset_size = len(dataset)
-    indices = list(range(dataset_size))
-    trainsplit = int(np.floor(train_split * dataset_size))
-    if shuffle_dataset:
-        np.random.seed(random_seed)
-        np.random.shuffle(indices)
-    train_indices, test_indices = indices[trainsplit:], indices[:trainsplit]
-    trainset_size = len(train_indices)
-    
-    valsplit = int(np.floor(validation_split * trainset_size))
-    train_indices, val_indices = train_indices[valsplit:], train_indices[:valsplit]
-
-    train_sampler = SubsetRandomSampler(train_indices)
-    valid_sampler = SubsetRandomSampler(val_indices)
-    test_sampler = SubsetRandomSampler(test_indices)
-
-    train_loader = DataLoader(dataset, batch_size = batchSize,
-                                        sampler = train_sampler)
-    validation_loader = DataLoader(dataset, batch_size = batchSize,
-                                        sampler = valid_sampler)
-    test_loader = DataLoader(dataset, batch_size = batchSize,
-                                        sampler = test_sampler)
-
-
-    dataloaders = {"train": train_loader, "val": validation_loader, "test": test_loader}
-
-    dataset_sizes = {"train": len(train_indices),
-                     "val": len(val_indices),
-                     "test": len(test_indices)}
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size = batchSize,
-                                             shuffle = True, num_workers = workers)
-
-    
-    return dataloaders, dataset_sizes, dataloader, class_names, dataset
-
-def trainLoop(model, criterion, optimizer, scheduler, dataloaders, dataset_sizes, dataloader, num_epochs = 25):
-    since = time.time()
-    
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-    trainAcc = []
-    trainLoss = []
-    validAcc = []
-    validLoss = []
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-        
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train() # Set model to training mode
-            else:
-                model.eval()  # Set model to evaluate mode
-            
-            running_loss = 0.0
-            running_corrects = 0
-            
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-                
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-                    
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-                        
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-                del(inputs)
-                del(labels)
-                
-            if phase == 'train':
-                scheduler.step()
-                
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-            
-            if phase == 'train':
-                trainAcc.append(epoch_acc)
-                trainLoss.append(epoch_loss)
-            else:
-                validAcc.append(epoch_acc)
-                validLoss.append(epoch_loss)
-                
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-            
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-                
-        print()
-        
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format( time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
-    
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model, [trainAcc, trainLoss], [validAcc, validLoss]
-
-def visualizeModel(model, num_images = 6):
+def visualize_model(model, num_images = 6):
     was_training = model.training
     model.eval()
     images_so_far = 0
@@ -185,70 +44,151 @@ def visualizeModel(model, num_images = 6):
                     model.train(mode = was_training)
                     return
         model.train(mode = was_training)
+
         
-def trainModel(model, modelName, criterion, optimizer, scheduler, dataType, figSize, batchSize):
-#     dataTypes = ['512', '768', 'shifted']
-    stats = {
-        '512': [],
-        '768': [],
-        'shifted': []
-    }
+def get_dataset(path, image_size):
+    dataset = datasets.ImageFolder(root = path,
+                                  transform = transforms.Compose([
+                                      transforms.Resize(image_size),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.5, 0.5, 0.5),
+                                                           (0.5, 0.5, 0.5))
+                                  ]))
+    return dataset
+
+def getData(data_type, folder, data_address = 'C:\\Users\\Patrick\\Documents\\GitHub\\ICDeepLearning\\Data',
+            random_seed = 42, shuffle_dataset = True, batch_size = 4, image_size = 224, workers = 1,
+            test_split = .2):
     
-    print("Training stage:{}".format(dataType))
-    dataloaders, dataset_sizes, dataloader, _, _ = getData(dataType, batchSize)
-    model, *stats[dataType] = trainLoop(model, criterion, optimizer, scheduler, dataloaders, dataset_sizes, dataloader, num_epochs = 10)
-    torch.save(model.state_dict(), 'Models\\'+modelName+'\\cca'+figSize+"\\model.pth")
-    torch.save(stats, 'Models\\'+modelName+'\\cca'+figSize+'\\stats'+dataType+'.pth')
+    figPath = data_address+'\\'+folder+'\\'+data_type
+
+    dataset = get_dataset(figPath, image_size)
+    class_names = dataset.classes
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    
+    if shuffle_dataset:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    
+    test_split_id = int(np.floor(test_split * dataset_size))
+    train_indices, test_indices = indices[test_split_id:], indices[:test_split_id]
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+
+    train_loader = DataLoader(dataset, batch_size = batch_size,
+                                        sampler = train_sampler)
+    test_loader = DataLoader(dataset, batch_size = batch_size,
+                                        sampler = test_sampler)
+
+
+    dataloaders = {"train": train_loader, "test": test_loader}
+
+    dataset_sizes = {"train": len(train_indices),
+                     "test": len(test_indices)}
+    
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size = batch_size,
+                                             shuffle = True, num_workers = workers)
+
+    
+    return dataloader, dataloaders, dataset_sizes, class_names
+
+def train(model, dataloaders, dataset_size, criterion, optimizer, scheduler, num_epochs = 10):
+    since = time.time()
+    
+    best_model_wgts = copy.deepcopy(model.state_dict())
+    best_acc = .0
+    trainAcc = []; trainLoss = []
+    testAcc  = []; testLoss  = []
+    
+    for epoch in range(num_epochs):
+        print('-'*2)
+        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
+        print('-'*5)
         
-    return model, stats
-
-modelDict = {
-    'resnet18': [models.resnet18(pretrained = True), 512],
-    'resnet34': [models.resnet34(pretrained = True), 512],
-    'resnet50': [models.resnet50(pretrained = True), 2048],
-    'resnet101': [models.resnet101(pretrained = True), 2048],
-    'resnet152': [models.resnet152(pretrained = True), 2048],
-    'densenet121': [models.densenet121(pretrained = True), 1024],
-    'densenet161': [models.densenet161(pretrained = True), 2208],
-    'densenet169': [models.densenet169(pretrained = True), 1664],
-    'densenet201': [models.densenet201(pretrained = True), 1920],
-    'alexnet': [models.alexnet(pretrained = True), 4096],
-    'googlenet': [models.googlenet(pretrained = True), 1024],
-    'vgg11': [models.vgg11(pretrained = True), 4096],
-    'vgg13': [models.vgg13(pretrained = True), 4096],
-    'vgg16': [models.vgg16(pretrained = True), 4096],
-    'vgg19': [models.vgg19(pretrained = True), 4096],
-    'mnasnet0_5': [models.mnasnet0_5(pretrained = True), 1280],
-    'mnasnet1_0': [models.mnasnet1_0(pretrained = True), 1280],
-    'resnext101_32x8d': [models.resnext101_32x8d(pretrained = True), 2048],
-    'resnext50_32x4d': [models.resnext50_32x4d(pretrained = True), 2048],
-    'shufflenet_v2_x0_5': [models.shufflenet_v2_x0_5(pretrained = True), 1024]
-}
-
-def lastLayer(modelType, model):
-    Dict = {
-        'resnet': lambda model: model.fc,
-        'densenet': lambda model: model.classifier,
-        'alexnet': lambda model: model.classifier[6],
-        'vgg': lambda model: model.classifier[6],
-        'googlenet': lambda model: model.fc,
-        'mnasnet': lambda model: model.classifier[1],
-        'resnext': lambda model: model.fc,
-        'shufflenet': lambda model: model.fc
-    }
-    return Dict[modelType](model)
-
-def autoTraining(modelType, modelSize, dataType, figSize, batchSize):
-    modelName = modelType+modelSize
-    numClasses = 5
-    model = modelDict[modelName][0]
-    numFeatures = modelDict[modelName][1]
-    classifier = lastLayer(modelType, model)
-    classifier = nn.Linear(numFeatures, numClasses)
-    model = model.to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum = 0.9)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size = 7, gamma = 0.1)
-    model, stats = trainModel(model, modelName, criterion, optimizer, scheduler, dataType, figSize, batchSize)
-#     del(model)
-    return model
+        # Each epoch as a training and testing phase
+        for phase in ['train', 'test']:
+            print('Stage: {}'.format(phase))
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+                
+            cumulative_loss = 0.0
+            cumulative_hits  = 0
+            
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                
+                optimizer.zero_grad()
+                
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                
+                cumulative_loss += loss.item()*inputs.size(0)
+                cumulative_hits += torch.sum(preds == labels.data)
+                del(inputs); del(labels)
+                
+            if phase == 'train':
+                scheduler.step()
+            
+            epoch_loss = cumulative_loss / dataset_size[phase]
+            epoch_acc  = cumulative_hits.double() / dataset_size[phase]
+            
+            if phase == 'train':
+                trainAcc.append(epoch_acc); trainLoss.append(epoch_loss)
+            else:
+                testAcc.append(epoch_acc); testLoss.append(epoch_loss)
+                
+            print('Loss: {:.4f} - Accuracy: {:.4f}'.format(epoch_loss, epoch_acc))
+            
+            if phase == 'test' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            
+    time_elapsed = time.time() - since
+    print('Best validation accuracy: {:4f}'.format(best_acc))
+    
+    model.load_state_dict(best_model_wgts)
+    return model, best_acc, time_elapsed
+                    
+def kfold_train(model, dataset, criterion, optimizer, scheduler, num_folds = 5, batch_size = 4, shuffle = False, save_model = False, num_epochs = 10):
+    kfold = KFold(num_folds, shuffle)
+    cumulative_best_acc = 0
+    cumulative_time = 0
+    
+    for fold, (train_index, test_index) in enumerate(kfold.split(dataset, dataset.targets)):
+        print('Fold {}'.format(fold))
+        train_subset = torch.utils.data.Subset(dataset, train_index)
+        test_subset  = torch.utils.data.Subset(dataset, test_index)
+        dataset_size = {}
+        dataset_size['train'] = len(train_subset) 
+        dataset_size['test'] = len(test_subset)
+        dataloaders  = {}
+        dataloaders['train'] = torch.utils.data.DataLoader(train_subset, batch_size = batch_size, shuffle = shuffle)
+        dataloaders['test']  = torch.utils.data.DataLoader(test_subset, batch_size = batch_size, shuffle = shuffle)
+        
+        _, fold_best_acc, time_elapsed = train(model, dataloaders, dataset_size, criterion, optimizer, scheduler, num_epochs = num_epochs)
+            
+        cumulative_time += time_elapsed    
+        cumulative_best_acc += fold_best_acc
+        print(cumulative_best_acc.item())
+        
+        print('')
+        
+    mean_acc = cumulative_best_acc.item() / num_folds
+    
+    print('Training complete in {:.0f}m {:.0f}s'.format(cumulative_time // 60, cumulative_time % 60))
+    print('Mean acc: {}'.format(mean_acc))
+    if save_model:
+        torch.save(model.state_dict, 'model.pth')
+    
+    return model, cumulative_best_acc, mean_acc
+        
