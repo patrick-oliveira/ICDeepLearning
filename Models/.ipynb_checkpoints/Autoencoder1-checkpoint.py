cb9ef261d97852f11ba019feb4a92a2c17800c2e
Patrick Oliveira <patrick.oliveira@aluno.ufabc.edu.br>
2020-10-04 18:10:13 -0300

    Minimizing autoencoder

--- Models/.ipynb_checkpoints/Autoencoder1-checkpoint.py
+++ Models/.ipynb_checkpoints/Autoencoder1-checkpoint.py
@@ -7,16 +7,16 @@ class Autoencoder(nn.Module):
         n1 = kernel_n
         s = stride
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 3, kernel_size = (1, 2*n1 + 1), padding = (0, n1), stride = (1, s)),
+            nn.Conv2d(1, 1, kernel_size = (1, 2*n1 + 1), padding = (0, n1), stride = (1, s)),
             nn.ReLU(inplace = True),
         )
         
         self.pool    = nn.MaxPool2d(kernel_size = (1, 2), stride = (1, 2), return_indices = True)
-        self.batch_norm = nn.BatchNorm2d(3)
+        self.batch_norm = nn.BatchNorm2d(1)
         self.unpool  = nn.MaxUnpool2d(kernel_size = (1, 2), stride = (1, 2))
         
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(3, 1, kernel_size = (1, 2*n1 + 1), padding = (0, n1), stride = (1, s), output_padding = (0, s - 1))
+            nn.ConvTranspose2d(1, 1, kernel_size = (1, 2*n1 + 1), padding = (0, n1), stride = (1, s), output_padding = (0, s - 1))
         )
         
     
