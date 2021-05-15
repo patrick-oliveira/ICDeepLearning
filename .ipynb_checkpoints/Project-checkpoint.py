8e96c91cedeae1a5113e7570e8270faf4c1fe8ea
RedRoboto <patrick.oliveira@aluno.ufabc.edu.br>
2021-05-15 05:38:25 -0300

    dataset processing

--- .ipynb_checkpoints/Project-checkpoint.py
+++ .ipynb_checkpoints/Project-checkpoint.py
@@ -11,15 +11,10 @@ class Project:
     """
         Basic information about the project.
     """
-    base_dir:            Path = Path(__file__).parents[0]
-    raw_ssvep_dir:       Path = base_dir.parent / 'BCI_ssvep_dataset'
-    data_dir:            Path = base_dir / 'Data'
-    base_series_dir:     Path = data_dir / 'originals'
-    combined_series_dir: Path = data_dir / 'combined'
-    images_dir:          Path = data_dir / 'images'
-    output_dir:          Path = base_dir / 'Outputs'
-    tsinghua_raw_dir:    Path = data_dir / 'tsinghua' / 'raw' 
-    tsinghua_cca_dir:     Path = data_dir / 'tsinghua' / 'cca'
+    base:            Path = Path(__file__).parents[0]
+    data:            Path = base / 'Data'
+    output:          Path = base / 'Outputs'
+    tsinghua:        Path = base / 'Tsinghua'
     
     # Adaptar para printar todos os diretórios
     
